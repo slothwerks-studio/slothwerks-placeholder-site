f6206b2fce008ca18e80df24c31f07ff44c8c540
Sloth I <github@slothwerks.com>
2018-12-21 07:30:24 -0500

    Added close window functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,39 @@
 // Dump DOM elements to variables
 const landingPageLogo = document.getElementById("landing-page-logo");
 const techCalendar = document.getElementById("tech-calendar");
-const enaCalendar = document.getElementById("ena-calendar");
+const communityCalendar = document.getElementById("ena-calendar");
 const audioArchives = document.getElementById("audio-archives");
 
 function showTechCalendar() {
   // Close any currently open views
   landingPageLogo.classList.add("hidden");
-  enaCalendar.classList.add("hidden");
+  communityCalendar.classList.add("hidden");
   audioArchives.classList.add("hidden");
   // Remove hidden class from tech calendar
   techCalendar.classList.remove("hidden");
 }
 
-function showArchives() {
+function closeTechCalendar() {
+  // Close tech calendar
+  techCalendar.classList.add("hidden");
+  // Go back to landing page image
+  landingPageLogo.classList.remove("hidden");
+}
+
+function showAudioArchives() {
   // Close any currently open views
   landingPageLogo.classList.add("hidden");
   techCalendar.classList.add("hidden");
-  enaCalendar.classList.add("hidden");
+  communityCalendar.classList.add("hidden");
   // Remove hidden class from tech calendar
   audioArchives.classList.remove("hidden");
+}
+
+function closeAudioArchives() {
+  // Close audio archives
+  audioArchives.classList.add("hidden");
+  // Go back to landing page image
+  landingPageLogo.classList.remove("hidden");
 }
 
 function showCommunityCalendar() {
@@ -28,5 +42,12 @@ function showCommunityCalendar() {
   techCalendar.classList.add("hidden");
   audioArchives.classList.add("hidden");
   // Remove hidden class from tech calendar
-  enaCalendar.classList.remove("hidden");
+  communityCalendar.classList.remove("hidden");
+}
+
+function closeCommunityCalendar() {
+  // Close ENA community calendar
+  communityCalendar.classList.add("hidden");
+  // Go back to landing page image
+  landingPageLogo.classList.remove("hidden");
 }
