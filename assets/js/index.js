b9490e05329710944ebbb62b7130c18ed853ceb7
Sloth I <github@slothwerks.com>
2018-12-21 07:21:22 -0500

    Added basic styles to primary sections to page; completed navigation JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,5 +11,22 @@ function showTechCalendar() {
   audioArchives.classList.add("hidden");
   // Remove hidden class from tech calendar
   techCalendar.classList.remove("hidden");
-  console.log(landingPageLogo.classList);
+}
+
+function showArchives() {
+  // Close any currently open views
+  landingPageLogo.classList.add("hidden");
+  techCalendar.classList.add("hidden");
+  enaCalendar.classList.add("hidden");
+  // Remove hidden class from tech calendar
+  audioArchives.classList.remove("hidden");
+}
+
+function showCommunityCalendar() {
+  // Close any currently open views
+  landingPageLogo.classList.add("hidden");
+  techCalendar.classList.add("hidden");
+  audioArchives.classList.add("hidden");
+  // Remove hidden class from tech calendar
+  enaCalendar.classList.remove("hidden");
 }
