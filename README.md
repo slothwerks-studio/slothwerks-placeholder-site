609b5de2de03f8d036c5d69be894c49e4614afd6
Sloth I <github@slothwerks.com>
2019-01-16 10:28:01 -0500

    Starting over with mobile-first.  Header in place with basic styles; nav built sans styles; basic footer in place.

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # SlothWerks LLC
 ## Placeholder Website
 
-A placeholder site for SlothWerks LLC until we develop our actual public-facing site with real content.
+### Why a placeholder website?
 
-Starting out with vanilla JS for the moment.  Theoretically this site will allow visitors to access the Grand Circus tech calendar, the ENA community calendar, and check out the old SlothWerks audio archives.  Maybe even send an email or call.
+Our production time right now is focused on our clients, and the workload is only increasing.  We do not have time to develop a full-fledged site with a portfolio, images, video, and other content.
+
+That said:  the current [SlothWerks website](http://www.slothwerks.com/) was built with Wordpress back in 2013.  It references a [Facebook page](https://www.facebook.com/slothwerks/) that is totally unused and includes legacy products we no longer offer.  Frankly, *anything* would be better than what we have right now.
+
+### Why build this on GitHub?
+
+This allows us to explore some of the newer options available on GitHub, such as [project boards](https://help.github.com/articles/about-project-boards/).  It also will force us to organize our information in a repo [wiki](https://github.com/slothwerks-studio/slothwerks-placeholder-site/wiki) and keep track of updates in [Issues](https://github.com/slothwerks-studio/slothwerks-placeholder-site/issues).
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,81 +1,10 @@
 // Dump DOM elements to variables
-const landingPageLogo = document.getElementById("landing-page-logo");
-const techCalendar = document.getElementById("tech-calendar");
-const communityCalendar = document.getElementById("ena-calendar");
-const audioArchives = document.getElementById("audio-archives");
+const menu = document.getElementById("menu");
 
-function showTechCalendar() {
-  // Close any currently open views
-  landingPageLogo.classList.add("hidden");
-  communityCalendar.classList.add("hidden");
-  audioArchives.classList.add("hidden");
-  // Remove hidden class from tech calendar
-  techCalendar.classList.remove("hidden");
-}
-
-function closeTechCalendar() {
-  // Close tech calendar
-  techCalendar.classList.add("hidden");
-  // Go back to landing page image
-  landingPageLogo.classList.remove("hidden");
-}
-
-function showAudioArchives() {
-  // Close any currently open views
-  landingPageLogo.classList.add("hidden");
-  techCalendar.classList.add("hidden");
-  communityCalendar.classList.add("hidden");
-  // Remove hidden class from tech calendar
-  audioArchives.classList.remove("hidden");
-}
-
-function closeAudioArchives() {
-  // Close audio archives
-  audioArchives.classList.add("hidden");
-  // Go back to landing page image
-  landingPageLogo.classList.remove("hidden");
-}
-
-function showCommunityCalendar() {
-  // Close any currently open views
-  landingPageLogo.classList.add("hidden");
-  techCalendar.classList.add("hidden");
-  audioArchives.classList.add("hidden");
-  // Remove hidden class from tech calendar
-  communityCalendar.classList.remove("hidden");
-}
-
-function closeCommunityCalendar() {
-  // Close ENA community calendar
-  communityCalendar.classList.add("hidden");
-  // Go back to landing page image
-  landingPageLogo.classList.remove("hidden");
-}
-
-// Display audio player based on id passed in via argument
-function showAudioPlayer(id) {
-  // Fetch audio players from the DOM
-  const hackShow2016 = document.getElementById("hack-show-2016");
-  const psychedelicBreakfast = document.getElementById("psychedelic-breakfast");
-  const arabianNights = document.getElementById("arabian-nights");
-  const arcoIris = document.getElementById("arco-iris");
-  // Fetch "now playing" message from the DOM
-  const nowPlaying = document.getElementById("now-playing");
-  // If the player is not the currently selected one, hide it
-  if (hackShow2016.id !== id) { hackShow2016.classList.add("hidden"); }
-  if (psychedelicBreakfast.id !== id) { psychedelicBreakfast.classList.add("hidden"); }
-  if (arabianNights.id !== id) { arabianNights.classList.add("hidden"); }
-  if (arcoIris.id !== id) { arcoIris.classList.add("hidden"); }
-  // Unhide currently selected player
-  const playerId = document.getElementById(id);
-  playerId.classList.remove("hidden");
-  // Create now playing message
-  let showName;
-  if (id === "hack-show-2016") { showName = "The Sloth Psychedelic HACK! Show (2016)"}
-  if (id === "psychedelic-breakfast") { showName = "Sloth's Psychedelic Breakfast (2007)"}
-  if (id === "arabian-nights") { showName = "1001 Arabian Nights (2005)"}
-  if (id === "arco-iris") { showName = "Sloth and Eric Newton Live at Arco Iris (1999)"}
-  const nowPlayingMessage = "Now playing: " + showName;
-  // Insert message into the DOM
-  nowPlaying.innerHTML = nowPlayingMessage;
+function toggleMenu() {
+  if (menu.className.includes("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
 }
