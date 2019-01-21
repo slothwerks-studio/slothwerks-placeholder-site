8a20b65cbe70684836b65305ec2a787017cd96d6
Sloth I <github@slothwerks.com>
2019-01-21 08:50:01 -0500

    Added basic menu styles and functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,34 @@
 // Dump DOM elements to variables
 const menu = document.getElementById("menu");
+const contentContainer = document.getElementById("contentContainer");
+const openMenuButton = document.getElementById("openMenuButton");
+const closeMenuButton = document.getElementById("closeMenuButton");
 
+// Toggle the nav menu
+// If not currently displayed, display the nav and hide the content container
+// Also flip the menu button to a "close" button
+// If currently displayed, show the content container and hide the nav menu
+// Flip the menu close button to standard "menu" button
 function toggleMenu() {
   if (menu.className.includes("hidden")) {
+    // Hide content container
+    contentContainer.classList.add("hidden");
+    // Show menu
     menu.classList.remove("hidden");
+    // Hide open menu button
+    openMenuButton.classList.add("hidden");
+    // Show close menu button
+    closeMenuButton.classList.remove("hidden");
   } else {
+    // Scroll to top of page
+    window.scrollTo(0,0);
+    // Hide menu
     menu.classList.add("hidden");
+    // Show content container
+    contentContainer.classList.remove("hidden");
+    // Hide close menu button
+    closeMenuButton.classList.add("hidden");
+    // Show open menu button
+    openMenuButton.classList.remove("hidden");
   }
 }
